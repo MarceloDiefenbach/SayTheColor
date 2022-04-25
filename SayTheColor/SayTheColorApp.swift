fccb85404366ada62a2d34425c7ab6f92d7a53bb
Marcelo <lelod15@gmail.com>
2022-04-25 09:06:51 -0300

    Initial Commit

---
+++ SayTheColor/SayTheColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayTheColorApp.swift
+//  SayTheColor
+//
+//  Created by Marcelo Diefenbach on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SayTheColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
